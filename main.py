8d6933ef6eefca0b1acb27c949b9addf3d5f228a
GingerTC <72882312+GingerTC@users.noreply.github.com>
2020-11-10 10:24:55 -0800

    Python code for binary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #import "Flask"
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 #create "Flask"
 app = Flask(__name__)
@@ -28,6 +28,38 @@ def rsaEX():
 @app.route('/rsa/about')
 def rsaAbout():
     return render_template("rsaAbout.html")
+
+@app.route("/bin_encrypt", methods=['GET','POST'])
+def encryption():
+    if request.method == 'POST':
+        form = request.form
+        B_text = form["bin1"]
+        result = ''.join(format(ord(i), 'b') for i in B_text)
+        return render_template("home.html", display = result)
+    return redirect("/home")
+
+@app.route("/bin_decrypt", methods=['GET','POST'])
+def decryption():
+    if request.method == 'POST':
+        form = request.form
+        def Binary_to_Text(binary):
+
+            binary1 = binary
+            decimal, d, n = 0, 0 , 0
+            while (binary != 0):
+                deci = binary % 10
+                decimal = decimal + deci * pow(2, d)
+                binary = binary//10
+                d += 1
+            return(decimal)
+        B_text = form["bin1"]
+        string = ' '
+        for d in range(0, len(B_text), 7):
+            temporary = int(B_text[d:d + 7])
+            decimal_data = Binary_to_Text(temporary)
+            string = string + chr(decimal_data)
+        return render_template("home.html", display = string)
+    return redirect("/home")
 
 #run file
 if __name__ == "__main__":
